806b086214bdbd9a77dcebdd770083a708dca1c1
Erran Carey <me@errancarey.com>
2014-01-02 02:30:23 -0600

    Create problems/gherkin/solution.js

---
+++ problems/gherkin/solution.js
@@ -0,0 +1,18 @@
+{
+  passed: {
+    scenarios: 0,
+    steps: 0
+  },
+  failed: {
+    scenarios: 0,
+    steps: 0
+  },
+  skipped: {
+    scenarios: 0,
+    steps: 0
+  },
+  pending: {
+    scenarios: 0,
+    steps: 0
+  }
+}
