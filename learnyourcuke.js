438ff9de1d355f47658992d2613ce656dc755775
Erran Carey <me@errancarey.com>
2014-01-01 22:44:30 -0600

    Create "bin" file and package.json

---
+++ learnyourcuke.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const Workshopper = require('workshopper')
+    , path        = require('path')
+
+Workshopper({
+    name              : 'learnyounode'
+  , title             : 'LEARN YOU THE NODE.JS FOR MUCH WIN!'
+  , appDir            : __dirname
+  , helpFile          : path.join(__dirname, 'help.txt')
+  , prerequisitesFile : path.join(__dirname, 'prerequisites.txt')
+  , creditsFile       : path.join(__dirname, 'credits.txt')
+}).init()
